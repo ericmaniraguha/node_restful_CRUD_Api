e78b8718dad1418f476a53a024d8022053f9d2fd
ericmaniraguha <ericmaniraguha@gmail.com>
2023-07-16 11:54:14 +0200

    done by creating models,the middleware, post route

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const express = require("express")
 const mongoose =  require("mongoose")
 const app = express()
 
+//create middleware to make our apps to understand json
+app.use(express.json())
+
 //create routes 
 app.get('/',(req, res)=>{
     res.send("Hello Node API, here it is...")
@@ -13,6 +16,14 @@ app.get('/',(req, res)=>{
 app.get('/blog',(req, res) =>{
     res.send("Hello blog, My name is Devtamin")
 })
+
+
+//route for saving data into the rdbsm
+app.post('/product',(req, res) =>{
+    console.log(req.body);
+    res.send(req.body)
+})
+
 
 
 mongoose.set("strictQuery",false )
